7331c3c82bba3755d8ea3b7fcf5675c2714265d6
Daniel Mediote <daniel_mediote@hotmail.com>
2019-05-31 20:00:02 -0400

    testando essa parada do commit

--- projeto/app/src/main/java/com/example/telasprojeto/TelaPrincipal.java
+++ projeto/app/src/main/java/com/example/telasprojeto/TelaPrincipal.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 public class TelaPrincipal extends AppCompatActivity implements View.OnClickListener{
 
 
+
     Button btnMedico, btnRemedios, btnPacientes, btnAtendimento;
 
     @Override
