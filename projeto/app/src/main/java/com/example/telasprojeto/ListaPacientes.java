376fca1588a4826464b9feb68f47b6e9bf598309
Daniel Mediote <daniel_mediote@hotmail.com>
2019-06-01 14:17:00 -0400

    Alterações das Telas

---
+++ projeto/app/src/main/java/com/example/telasprojeto/ListaPacientes.java
@@ -0,0 +1,13 @@
+package com.example.telasprojeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaPacientes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacientes);
+    }
+}
