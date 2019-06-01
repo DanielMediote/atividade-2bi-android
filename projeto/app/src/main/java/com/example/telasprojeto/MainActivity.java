71ba0bb70567770aaf778a7ce41ac542bfc7df15
cleiton-D <cleitonahnerth@gmail.com>
2019-06-01 15:56:30 -0400

    classes de criação de banco de dados

--- projeto/app/src/main/java/com/example/telasprojeto/MainActivity.java
+++ projeto/app/src/main/java/com/example/telasprojeto/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.telasprojeto;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.example.db.DBHelper;
 
 public class MainActivity extends AppCompatActivity{
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DBHelper dbHelp;
+        try {
+            dbHelp = new DBHelper(this);
+        } catch (SQLiteException e){
+            Toast.makeText(this, "Falha na conexão!", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+
+
     }
 
     public void onClick(View v){
