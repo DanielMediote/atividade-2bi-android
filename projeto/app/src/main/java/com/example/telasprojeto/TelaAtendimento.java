4e6459d5814aa00948cf946e1bbbb0dea7cbc56d
Daniel Mediote <daniel_mediote@hotmail.com>
2019-06-02 22:48:24 -0400

    Alterações da tela de Atendimento

--- projeto/app/src/main/java/com/example/telasprojeto/TelaAtendimento.java
+++ projeto/app/src/main/java/com/example/telasprojeto/TelaAtendimento.java
@@ -13,11 +13,13 @@ public class TelaAtendimento extends AppCompatActivity {
 
     ListView listaAtendimento;
     Button btnProximo, btnVoltar;
-    ArrayAdapter<String> arrayAdapter;
+    ArrayAdapter<String> arrayAdapterPacientes, arrayAdapterRemedios;
+    Integer etapaAtendimento = 0;
 
     //TESTE
     String[] pacientes = {"Paciente 1", "Paciente 2", "Paciente 3"};
     String[] remedios = {"Remedio 1", "Remedio 2", "Remedio 3"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,28 +29,39 @@ public class TelaAtendimento extends AppCompatActivity {
         btnVoltar = findViewById(R.id.btnVoltar);
         listaAtendimento = findViewById(R.id.listView_Atendimento);
 
-        //CARREGAR OS PACIENTES NA LISTVIEW
-        arrayAdapter = new ArrayAdapter<String>
-                (this, android.R.layout.simple_list_item_1, pacientes);
+        arrayAdapterPacientes = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, pacientes);
+        arrayAdapterRemedios = new ArrayAdapter<String>(this,android.R.layout.select_dialog_item, remedios);
 
-        listaAtendimento.setAdapter(arrayAdapter);
+        //AO CLICK MUDAR A CORD DO ITEM SELECIONADO
+        listaAtendimento.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                for (int j = 0; j < parent.getChildCount(); j++)
+                    parent.getChildAt(j).setBackgroundColor(Color.TRANSPARENT);
+                view.setBackgroundColor(Color.parseColor("#803D5AFE"));
+            }
+        });
 
+        listaAtendimento.setAdapter(arrayAdapterPacientes);
         btnVoltar.setVisibility(View.GONE);
     }
 
     public void onClick(View v){
         if (v == btnProximo){
-            btnVoltar.setVisibility(View.VISIBLE);
-
-            //CARREGR OS REMEDIOS NA LISTVIEW
-            arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_item, remedios);
-            listaAtendimento.setAdapter(arrayAdapter);
+            if (etapaAtendimento == 0){
+                btnVoltar.setVisibility(View.VISIBLE);
+                listaAtendimento.setAdapter(arrayAdapterRemedios);
+            }
+            etapaAtendimento++;
         }else if(v == btnVoltar){
-
+            if (etapaAtendimento == 1){
+                btnVoltar.setVisibility(View.GONE);
+                listaAtendimento.setAdapter(arrayAdapterPacientes);
+            }
+            etapaAtendimento--;
         }else{
 
         }
-
     }
 
 
